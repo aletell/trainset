6ef29e26e862cca47642956222233c03cdfd8d70
aletell <46355246+aletell@users.noreply.github.com>
2025-06-05 13:20:37 +0200

    Update landing page design and credits

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,33 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import vuePlugin from 'eslint-plugin-vue';
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  resolvePluginsRelativeTo: __dirname
-});
+import js from '@eslint/js';
+import vue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
 
 export default [
-  ...compat.config({
-    env: { browser: true },
-    parserOptions: {
-      parser: 'babel-eslint'
-    },
-    plugins: ['vue'],
-    extends: ['plugin:vue/essential', 'standard'],
-    rules: {
-      'generator-star-spacing': 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-    }
-  }),
+  js.configs.recommended,
   {
     files: ['src/**/*.{js,vue}'],
-    ignores: ['build/**', 'config/**', 'dist/**', '*.js']
+    ignores: ['build/**', 'config/**', 'dist/**'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: 'babel-eslint',
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      },
+      globals: {
+        $: 'readonly',
+        jQuery: 'readonly'
+      }
+    },
+    plugins: { vue },
+    rules: {
+      'no-debugger': 'off',
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      'no-cond-assign': 'off',
+      'no-extra-boolean-cast': 'off',
+      'no-empty': 'off',
+      'no-self-assign': 'off'
+    }
   }
 ];
